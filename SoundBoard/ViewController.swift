e0c850ea51feadcc3ae06036b1208c1bea9b84aa
reynaldoperalta <reynaldopema14@gmail.com>
2021-10-20 12:01:16 -0700

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by reynaldo peralta marquez on 10/20/21.
+//  Copyright © 2021 reynaldo peralta marquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
